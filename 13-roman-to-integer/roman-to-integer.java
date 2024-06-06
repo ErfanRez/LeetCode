4802424fe3c7d5859bf02fc4ef286c667e3b6a4c
Erfan Rezaei <100679623+ErfanRez@users.noreply.github.com>
2024-06-06 23:19:36 +0330

    Time: 4 ms (66.83%) | Memory: 44.5 MB (73.45%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.java
+++ 13-roman-to-integer/roman-to-integer.java
@@ -2,19 +2,19 @@ import java.util.*;
 
 class Solution {
     public int romanToInt(String s) {
-        Map<String, Integer> romanToInt = new HashMap<>();
-        romanToInt.put("I", 1);
-        romanToInt.put("V", 5);
-        romanToInt.put("X", 10);
-        romanToInt.put("L", 50);
-        romanToInt.put("C", 100);
-        romanToInt.put("D", 500);
-        romanToInt.put("M" , 1000);
+        Map<Character, Integer> romanToInt = new HashMap<>();
+        romanToInt.put('I', 1);
+        romanToInt.put('V', 5);
+        romanToInt.put('X', 10);
+        romanToInt.put('L', 50);
+        romanToInt.put('C', 100);
+        romanToInt.put('D', 500);
+        romanToInt.put('M' , 1000);
         int sum = 0;
         int i = s.length() - 1;
         while(i >= 0){
-            int x = romanToInt.get(String.valueOf(s.charAt(i)));
-            int y = i > 0 ? romanToInt.get(String.valueOf(s.charAt(i-1))) : 0; 
+            int x = romanToInt.get(s.charAt(i));
+            int y = i > 0 ? romanToInt.get(s.charAt(i-1)) : 0; 
             if (x > y){
                 sum += (x-y);
                 i -= 2;
