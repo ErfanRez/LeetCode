543593d8a2d8440929879509cc8c11abb1462545
Erfan Rezaei <100679623+ErfanRez@users.noreply.github.com>
2024-06-11 01:11:36 +0330

    Time: 11 ms (72.57%) | Memory: 58.1 MB (54.74%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -1,16 +1,11 @@
-import java.util.*;
-
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        Set<Integer> set = new HashSet<>();
-        for(int num : nums){
-            if(set.contains(num)){
+        HashSet<Integer> seen = new HashSet<>();
+        for (int num : nums) {
+            if (seen.contains(num))
                 return true;
-            } else {
-                set.add(num);
-            }
+            seen.add(num);
         }
-
         return false;
     }
 }
