053860d8624e9e37ab84a796ed58476bce056672
Erfan Rezaei <100679623+ErfanRez@users.noreply.github.com>
2024-06-14 20:02:21 +0330

    Time: 1 ms (99.77%) | Memory: 42.9 MB (81.22%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,19 +1,19 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        Map<Character, Integer> count = new HashMap<>();
+        int[] count = new int[26];
         
         // Count the frequency of characters in string s
         for (char x : s.toCharArray()) {
-            count.put(x, count.getOrDefault(x, 0) + 1);
+            count[x - 'a']++;
         }
         
         // Decrement the frequency of characters in string t
         for (char x : t.toCharArray()) {
-            count.put(x, count.getOrDefault(x, 0) - 1);
+            count[x - 'a']--;
         }
         
         // Check if any character has non-zero frequency
-        for (int val : count.values()) {
+        for (int val : count) {
             if (val != 0) {
                 return false;
             }
@@ -34,3 +34,31 @@ class Solution {
 
 //         return Arrays.equals(charS, charT);
 //     }
+
+///////////////////////////////////////////////////////////////////
+
+// Using Hash Table: Big-O = O(n)
+
+// public boolean isAnagram(String s, String t) {
+//         Map<Character, Integer> count = new HashMap<>();
+        
+//         // Count the frequency of characters in string s
+//         for (char x : s.toCharArray()) {
+//             count.put(x, count.getOrDefault(x, 0) + 1);
+//         }
+        
+//         // Decrement the frequency of characters in string t
+//         for (char x : t.toCharArray()) {
+//             count.put(x, count.getOrDefault(x, 0) - 1);
+//         }
+        
+//         // Check if any character has non-zero frequency
+//         for (int val : count.values()) {
+//             if (val != 0) {
+//                 return false;
+//             }
+//         }
+        
+//         return true;
+//     }
+// }
