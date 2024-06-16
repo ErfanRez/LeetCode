a398e4ede82a10aa588afd1760484c6412c8f457
Erfan Rezaei <100679623+ErfanRez@users.noreply.github.com>
2024-06-16 23:35:11 +0330

    Time: 0 ms (100.00%) | Memory: 41.2 MB (68.86%) - LeetSync

---
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        String perfix = strs[0];
+
+        for(String s : strs){
+            while(s.indexOf(perfix) != 0){
+                perfix = perfix.substring(0, perfix.length() - 1);
+            }
+        }
+
+        return perfix;
+    }
+}
