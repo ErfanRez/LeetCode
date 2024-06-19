e4c52023f98a6f145d93d0736e0a5a082d52c5f5
Erfan Rezaei <100679623+ErfanRez@users.noreply.github.com>
2024-06-19 23:03:27 +0330

    Time: 0 ms (100.00%) | Memory: 41.5 MB (69.20%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.java
+++ 58-length-of-last-word/length-of-last-word.java
@@ -1,6 +1,24 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        String[] words = s.split(" ");
-        return words[words.length - 1].length();
+        s = s.trim();
+        
+        int length = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) != ' ') {
+                length++;
+            }
+            else if (length > 0) {
+                break;
+            }
+        }
+        
+        return length;
     }
 }
+
+// My way:
+
+// public int lengthOfLastWord(String s) {
+//         String[] words = s.split(" ");
+//         return words[words.length - 1].length();
+//     }
