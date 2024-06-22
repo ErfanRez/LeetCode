06dc76748ad2d169697c1974d4ae0e9491e18693
Erfan Rezaei <100679623+ErfanRez@users.noreply.github.com>
2024-06-22 23:32:55 +0330

    Time: 0 ms (100.00%) | Memory: 42.6 MB (20.21%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.java
+++ 206-reverse-linked-list/reverse-linked-list.java
@@ -8,24 +8,57 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
+ // Iterative solution:
 class Solution {
     public ListNode reverseList(ListNode head) {
-        ListNode dummyHead = new ListNode(0);
-        ListNode p = head;
-        ListNode current = dummyHead;
-        Stack<Integer> stack = new Stack<>();
+        ListNode current = head;
+        ListNode prev = null;
 
-        while( p != null ){
-            stack.push(p.val);
-            p = p.next;
+        while( current != null ){
+            ListNode temp = current.next;
+            current.next = prev;
+            prev = current;
+            current = temp;
         }
 
-        while(!stack.isEmpty()){
-            current.next = new ListNode(stack.pop());
-            current = current.next;
-        }
-
-        return dummyHead.next;
-        
+        return prev;
     }
 }
+
+//Recursive Solution:
+
+// public ListNode reverseList(ListNode head) {
+//         if (head == null || head.next == null) {
+//             return head;
+//         }
+
+//         ListNode newHead = reverseList(head.next);
+//         head.next.next = head;
+//         head.next = null;
+//         return newHead;        
+//   }
+
+/////////////////////////////////////////////////////////////////////
+
+//My way (Less optimized):
+
+// public ListNode reverseList(ListNode head) {
+//         ListNode dummyHead = new ListNode(0);
+//         ListNode p = head;
+//         ListNode current = dummyHead;
+//         Stack<Integer> stack = new Stack<>();
+
+//         while( p != null ){
+//             stack.push(p.val);
+//             p = p.next;
+//         }
+
+//         while(!stack.isEmpty()){
+//             current.next = new ListNode(stack.pop());
+//             current = current.next;
+//         }
+
+//         return dummyHead.next;
+        
+//     }
