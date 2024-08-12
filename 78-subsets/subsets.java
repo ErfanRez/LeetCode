8b8f40a59983dc816aa7113e2741fa10c594c655
Erfan Rezaei <100679623+ErfanRez@users.noreply.github.com>
2024-08-12 20:37:50 +0330

    Time: 1 ms (53.01%) | Memory: 42.4 MB (91.18%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,19 +1,25 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
-        backtrack(nums, 0, new ArrayList<>(), result);
-        return result;
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> subset = new ArrayList<>();
+
+        createSubset(nums, 0, res, subset);
+        return res;        
     }
-    
-    private void backtrack(int[] nums, int start, List<Integer> path, List<List<Integer>> result) {
-        result.add(new ArrayList<>(path));
-        for (int i = start; i < nums.length; i++) {
-            path.add(nums[i]);
-            backtrack(nums, i + 1, path, result);
-            path.remove(path.size() - 1);
+
+    private void createSubset(int[] nums, int index, List<List<Integer>> res, List<Integer> subset) {
+        if (index == nums.length) {
+            res.add(new ArrayList<>(subset));
+            return;
         }
-    }
-}
+
+        subset.add(nums[index]);
+        createSubset(nums, index + 1, res, subset);
+
+        subset.remove(subset.size() - 1);
+        createSubset(nums, index + 1, res, subset);
+    }    
+} // Big-O = O(2^n)
 
 //Iterative Approach:
 
@@ -32,3 +38,21 @@ class Solution {
 //         return result;
 //     }
 // }
+
+
+// Another rescursive way:
+
+// public List<List<Integer>> subsets(int[] nums) {
+//         List<List<Integer>> result = new ArrayList<>();
+//         backtrack(nums, 0, new ArrayList<>(), result);
+//         return result;
+//     }
+    
+//     private void backtrack(int[] nums, int start, List<Integer> path, List<List<Integer>> result) {
+//         result.add(new ArrayList<>(path));
+//         for (int i = start; i < nums.length; i++) {
+//             path.add(nums[i]);
+//             backtrack(nums, i + 1, path, result);
+//             path.remove(path.size() - 1);
+//         }
+//     }
