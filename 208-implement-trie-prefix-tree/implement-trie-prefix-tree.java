f288072a5b8b7b87691a2a74c6589d59b84ff9f3
Erfan Rezaei <100679623+ErfanRez@users.noreply.github.com>
2024-09-10 17:56:17 +0330

    Time: 34 ms (77.28%) | Memory: 55.3 MB (58.56%) - LeetSync

--- 208-implement-trie-prefix-tree/implement-trie-prefix-tree.java
+++ 208-implement-trie-prefix-tree/implement-trie-prefix-tree.java
@@ -44,3 +44,11 @@ class Trie {
         return true;
     }
 }
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie* obj = new Trie();
+ * obj->insert(word);
+ * bool param_2 = obj->search(word);
+ * bool param_3 = obj->startsWith(prefix);
+ */
