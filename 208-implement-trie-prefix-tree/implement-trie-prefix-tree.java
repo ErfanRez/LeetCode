cd1f0a9ea6533a28d7e1155998ca7966a33710f5
Erfan Rezaei <100679623+ErfanRez@users.noreply.github.com>
2024-09-10 13:56:40 +0330

    Time: 33 ms (85.70%) | Memory: 55.1 MB (73.96%) - LeetSync

---
+++ 208-implement-trie-prefix-tree/implement-trie-prefix-tree.java
@@ -0,0 +1,46 @@
+class Trie {
+    public Trie[] trie;
+    public boolean flag;
+
+    public Trie() {
+        trie = new Trie[26];
+        flag = false;
+    }
+
+    public void insert(String word) {
+        Trie node = this;
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            int ind = c - 'a';
+            if (node.trie[ind] == null) {
+                node.trie[ind] = new Trie();
+            }
+            node = node.trie[ind];
+        }
+        node.flag = true;
+    }
+
+    public boolean search(String word) {
+        Trie node = this;
+        for (int i = 0; i < word.length(); i++) {
+            int ind = word.charAt(i) - 'a';
+            if (node.trie[ind] == null) {
+                return false;
+            }
+            node = node.trie[ind];
+        }
+        return node.flag;
+    }
+
+    public boolean startsWith(String prefix) {
+        Trie node = this;
+        for (int i = 0; i < prefix.length(); i++) {
+            int ind = prefix.charAt(i) - 'a';
+            if (node.trie[ind] == null) {
+                return false;
+            }
+            node = node.trie[ind];
+        }
+        return true;
+    }
+}
