01b0eaf9976f07fa0cad6e64ee6416d9aa723edb
Erfan Rezaei <100679623+ErfanRez@users.noreply.github.com>
2024-11-15 00:07:16 +0330

    Time: 0 ms (100.00%) | Memory: 41.6 MB (48.35%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.java
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.java
@@ -8,51 +8,75 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-class Solution {
-    public ListNode removeLast(ListNode head){
-        ListNode p = head;
 
-        while(p.next.next != null){
-            p = p.next;
-        }
-
-        p.next = null;
-
-        return head;
-    }
-
-    public ListNode removeFirst(ListNode head){
-        return head = head.next;
-    }
-
-    public int size(ListNode head){
-        int size = 0;
-        ListNode p = head;
-
-        while(p != null){
-            p = p.next;
-            size++;
-        }
-
-        return size;
-    }
-
+ class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        int size = size(head);
+        ListNode res = new ListNode(0, head);
+        ListNode dummy = res;
 
-        if(size == 1) return null;
-        if(n == 1) return removeLast(head);
-
-        if(n == size) return removeFirst(head);
-
-        ListNode p = head;
-        for(int i = 1; i < size - n; i++){
-            p = p.next;
+        for (int i = 0; i < n; i++) {
+            head = head.next;
         }
 
-        p.next = p.next.next;
+        while (head != null) {
+            head = head.next;
+            dummy = dummy.next;
+        }
 
-        return head;
+        dummy.next = dummy.next.next;
 
+        return res.next;        
     }
 }
+
+
+//My solution (Space more optimized):
+
+// class Solution {
+//     public ListNode removeLast(ListNode head){
+//         ListNode p = head;
+
+//         while(p.next.next != null){
+//             p = p.next;
+//         }
+
+//         p.next = null;
+
+//         return head;
+//     }
+
+//     public ListNode removeFirst(ListNode head){
+//         return head = head.next;
+//     }
+
+//     public int size(ListNode head){
+//         int size = 0;
+//         ListNode p = head;
+
+//         while(p != null){
+//             p = p.next;
+//             size++;
+//         }
+
+//         return size;
+//     }
+
+//     public ListNode removeNthFromEnd(ListNode head, int n) {
+//         int size = size(head);
+
+//         if(size == 1) return null;
+//         if(n == 1) return removeLast(head);
+
+//         if(n == size) return removeFirst(head);
+
+//         ListNode p = head;
+//         for(int i = 1; i < size - n; i++){
+//             p = p.next;
+//         }
+
+//         p.next = p.next.next;
+
+//         return head;
+
+//     }
+// }
