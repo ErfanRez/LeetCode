0327f90e86b6786c344ed1d9b2da7628d7cca517
Erfan Rezaei <100679623+ErfanRez@users.noreply.github.com>
2024-11-23 02:47:07 +0330

    Time: 0 ms (100.00%) | Memory: 40.6 MB (67.30%) - LeetSync

--- 231-power-of-two/power-of-two.java
+++ 231-power-of-two/power-of-two.java
@@ -1,23 +1,25 @@
 class Solution {
-    public boolean isPowerOfTwo(int n) {
-        if( n <= 0) return false;
+    //Recursive approach, Big-O = O(logn)
+    public boolean isPowerOfTwo(int n) { 
+        if(n <= 0) return false;
         if(n == 1) return true;
-        // multuple of 2 will only have a single 1 in their binary representation.
-        //Big-O = O(1)
-        int cnt = Integer.bitCount(n);
 
-        return cnt == 1;
-
+        //We divide the number by 2 if we reach 1 is power of two otherwise it's not.
+        return (n % 2 == 0) && isPowerOfTwo(n / 2); 
         
     }
 
-    //Recursive approach, O(logn)
-    // public boolean isPowerOfTwo(int n) { 
-    //     if(n <= 0) return false;
+    //Using binary bits
+    // public boolean isPowerOfTwo(int n) {
+    //     if( n <= 0) return false;
     //     if(n == 1) return true;
+    //     // multuple of 2 will only have a single 1 in their binary representation.
+    //     //Big-O = O(1)
+    //     int cnt = Integer.bitCount(n);
 
-    //     //We divide the number by 2 if we reach 1 is power of two otherwise it's not.
-    //     return (n % 2 == 0) && isPowerOfTwo(n / 2); 
+    //     return cnt == 1;
+
         
     // }
+
 }
