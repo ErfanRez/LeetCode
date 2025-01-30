e4ff5b85739ba192bc2519ba72bfdbfcf00206f0
Erfan Rezaei <100679623+ErfanRez@users.noreply.github.com>
2025-01-30 23:40:00 +0330

    Time: 31 ms (91.44%), Space: 17.7 MB (48.34%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.py
+++ 1013-fibonacci-number/1013-fibonacci-number.py
@@ -2,6 +2,8 @@ class Solution:
     def fib(self, n: int) -> int:
         if n == 0:
             return 0
+        if n == 1:
+            return 1
 
         dp = [0] * (n + 1)
         dp[1] = 1
