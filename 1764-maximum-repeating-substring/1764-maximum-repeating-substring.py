21b71c67de6d8d4ab4370998ba81432255874118
Erfan Rezaei <100679623+ErfanRez@users.noreply.github.com>
2025-02-07 22:57:52 +0330

    Time: 0 ms (100%), Space: 17.7 MB (42.6%) - LeetHub

--- 1764-maximum-repeating-substring/1764-maximum-repeating-substring.py
+++ 1764-maximum-repeating-substring/1764-maximum-repeating-substring.py
@@ -3,10 +3,8 @@ class Solution:
         if not word:
             return 0
         k = 0
-        max_count = 0
         temp_word = word
         while temp_word in sequence:
             k += 1
             temp_word += word
-            max_count = max(max_count, k)
-        return max_count
+        return k
