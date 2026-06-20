b9778a5f8b985dbff14b10905bf832e66fac3336
Erfan Rezaei <100679623+ErfanRez@users.noreply.github.com>
2026-06-20 23:48:44 +0330

    Time: 437 ms (78.13%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,13 +1,13 @@
 # Write your MySQL query statement below
--- select B.id from weather A
---     join weather B 
---     on A.recordDate = DATE_SUB(B.recordDate, INTERVAL 1 DAY)
---     where B.temperature > A.temperature;
+select B.id from weather A
+    join weather B 
+    on A.recordDate = DATE_SUB(B.recordDate, INTERVAL 1 DAY)
+    where B.temperature > A.temperature;
 
-SELECT id
-FROM weather w1
-WHERE temperature > (
-    SELECT temperature
-    FROM weather w2
-    WHERE w2.recordDate = DATE_SUB(w1.recordDate, INTERVAL 1 DAY)
-);
+-- SELECT id
+-- FROM weather w1
+-- WHERE temperature > (
+--     SELECT temperature
+--     FROM weather w2
+--     WHERE w2.recordDate = DATE_SUB(w1.recordDate, INTERVAL 1 DAY)
+-- );
